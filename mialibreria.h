2253d543a4ec0b0ff1e85df335f0edd0ace32d77
Matteo Calosci <matteo1789@live.it>
2015-02-08 13:19:26 +0100

    correzione

---
+++ mialibreria.h
@@ -0,0 +1,6 @@
+int EsistonoSoluzioniReali(float a, float b, float c);
+float CalcolaDeterminante(float a, float b, float c);
+float CalcolaSoluzione1(float a, float b, float c);
+float CalcolaSoluzione2(float a, float b, float c);
+void StampaRisultato(float x1, float x2);
+
